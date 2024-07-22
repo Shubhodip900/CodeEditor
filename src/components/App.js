40c46181553be58884ac62faa7e60ab677d9e553
Shubhodip Pal <palshubhodip9@gmail.com>
2024-07-22 22:43:03 +0530

    changes in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -48,7 +48,7 @@ function App() {
         <iframe
           srcDoc={srcDoc}
           title="output"
-          sandbox="allow-scripts"
+          sandbox="allow-forms allow-scripts allow-same-origin"
           frameBorder="0"
           width="100%"
           height="100%"
